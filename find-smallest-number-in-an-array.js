a32f6a6da68eacae53bc90c303f0b5b361bd8248
Santosh Molleti <santosh.molleti@accordion.com>
2025-08-15 16:33:56 +0530

    find smallest number in an array

---
+++ find-smallest-number-in-an-array.js
@@ -0,0 +1,10 @@
+function findSmallestNumberInAnArray(arr){
+    if(arguments.length!==1 || !Array.isArray(arr) || arr.length===0) return 'Invalid'
+    num=Infinity
+    for(let i=0;i<arr.length;i++){
+        if(typeof(arr[i])!=="number" || !isFinite(arr[i])|| isNaN(arr[i])) return 'Invalid'
+        if(num>arr[i]) num = arr[i]
+    }
+    return num
+}
+console.log(findSmallestNumberInAnArray([1,4,-5,8,10]))
