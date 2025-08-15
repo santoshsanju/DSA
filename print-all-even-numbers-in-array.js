f3144988d97be62d7e83c26747743364b2d01a4a
Santosh Molleti <santosh.molleti@accordion.com>
2025-08-15 16:19:08 +0530

    for loop

--- print-all-even-numbers-in-array.js
+++ print-all-even-numbers-in-array.js
@@ -1,5 +1,12 @@
 function printAllEvenNumbersInAnArray(arr){
-    if(typeof(arr)==="object" && Array.isArray(arr) && arr.length>0 && arr.every(i=>typeof(i)==="number") && arr.filter(i=>!isFinite(i) || isNaN(i)).length==0) return arr.filter(i=>i%2===0)
-    return 'Invalid'
+    // if(typeof(arr)==="object" && Array.isArray(arr) && arr.length>0 && arr.every(i=>typeof(i)==="number") && arr.filter(i=>!isFinite(i) || isNaN(i)).length==0) return arr.filter(i=>i%2===0)
+    // return 'Invalid'
+    if(arguments.length!==1 || !Array.isArray(arr) || arr.length<=0) return 'Invalid'
+    ans=[]
+    for(let i=0;i<arr.length;i++){
+        if(typeof(arr[i])!=='number'|| !isFinite(arr[i])|| isNaN(arr[i])) return 'Invalid'
+        if(arr[i]%2===0) ans.push(arr[i])
+    }
+    return ans
 }
-console.log(printAllEvenNumbersInAnArray([Infinity, 2]))
+console.log(printAllEvenNumbersInAnArray([2]))
